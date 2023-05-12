daeceef7ba503fd4be4fea97a5e80ea76e145037
Eloi <recycled_loops@hotmail.com>
2023-05-12 14:05:09 +0200

    set Next and provider

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,95 +1,185 @@
-import Image from 'next/image'
-import styles from './page.module.css'
+"use client";
+import React, { useEffect, useState } from "react";
+import { Alchemy, Network } from "alchemy-sdk";
+import {
+  ChakraProvider,
+  Flex,
+  Box,
+  Text,
+  Button,
+  List,
+  ListItem,
+  Input,
+  Heading,
+  ListIcon,
+} from "@chakra-ui/react";
 
-export default function Home() {
+const settings = {
+  apiKey: process.env.REACT_APP_ALCHEMY_API_KEY,
+  network: Network.ETH_MAINNET,
+};
+
+const alchemy = new Alchemy(settings);
+
+function App() {
+  const [blockNumber, setBlockNumber] = useState<number | undefined>();
+  const [blockTransactions, toggleBlockTransactions] = useState(false);
+  const [blockDetails, setBlockDetails] = useState();
+  const [singleTransactionInfo, setSingleTransactionInfo] = useState();
+  const [requestHash, setRequestHash] = useState("");
+  const [balance, setBalance] = useState("");
+
+  useEffect(() => {
+    async function getBlockNumber() {
+      setBlockNumber(await alchemy.core.getBlockNumber()); // .getBlockNumber returns the block number of the most recently mined block
+    }
+
+    getBlockNumber();
+  }, []);
+
+  useEffect(() => {
+    async function getBlockTransactions() {
+      setBlockDetails(await alchemy.core.getBlock(blockNumber));
+    }
+    getBlockTransactions();
+  }, [blockNumber]);
+  // console.log('blockDetails', blockDetails);
+
+  const handleToggleBlockTransactions = () => {
+    toggleBlockTransactions(!blockTransactions);
+  };
+
+  const handleTransactionInfo = async (index: string | number) => {
+    await alchemy.core
+      .getTransactionReceipt(blockDetails?.transactions[index])
+      .then(setSingleTransactionInfo);
+  };
+
+  const handleInputValue = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setRequestHash(e.target.value);
+  };
+
+  const handleBalance = async () => {
+    await alchemy.core
+      .getBalance(requestHash)
+      .then((balance) => setBalance(balance.toString()));
+  };
+
+  const handleOnKeyPressed = (event: React.KeyboardEvent<HTMLInputElement>) => {
+    if (event.key === "Enter") {
+      handleBalance();
+    }
+  };
+
+  const handleGetLatestTransactions = async () =>
+    setBlockNumber(await alchemy.core.getBlockNumber());
+
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
+    <React.Fragment>
+      <Flex>
+        <Flex direction="column" w="100vw" p="2">
+          {/* <Box flex="1" h="300px">
+              <Text>hello</Text>
+            </Box> */}
+          <Button
+            colorScheme="teal"
+            variant="solid"
+            mb="2"
+            onClick={handleGetLatestTransactions}
           >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
+            Get most recently mined block
+          </Button>
+          {/* <Center bg="#262525" h="100vh" color="white"> */}
+          <Flex bg="#262525" h="100vh" color="white" justify="center" pt="6">
+            <Flex direction="column">
+              <Box
+                style={{
+                  border: "1px solid black",
+                  overflowY: "scroll",
+                  borderRadius: "15px",
+                }}
+                w="1100px"
+                h="600px"
+                align="center"
+              >
+                <Heading as="h6" mb="4">
+                  Block Number: {blockDetails?.number}
+                </Heading>
+                <Text mb="2">Click to display block transactions:</Text>
+                <Button
+                  size="md"
+                  colorScheme="teal"
+                  mb="3"
+                  onClick={handleToggleBlockTransactions}
+                >
+                  {blockDetails?.number}
+                </Button>
+                <Flex justify="center" align="center">
+                  <Text as="b">Parent hash: </Text>
+                  <Text>{blockDetails?.parentHash}</Text>
+                </Flex>
+                <Flex justify="start" mt="5">
+                  <List>
+                    <Text mb="3">Block transactions:</Text>
+                    {blockTransactions
+                      ? blockDetails?.transactions?.map(
+                          (
+                            item: string | undefined,
+                            index: React.Key | null | undefined
+                          ) => {
+                            return (
+                              <ListItem>
+                                <ListIcon as="CheckIcon" color="green.500" />
+                                <Button
+                                  size="sm"
+                                  colorScheme="yellow"
+                                  mb="1"
+                                  onClick={() => handleTransactionInfo(index)}
+                                >
+                                  {item}
+                                </Button>
+                              </ListItem>
+                            );
+                          }
+                        )
+                      : null}
+                  </List>
+                </Flex>
+              </Box>
+              <Flex direction="column" mt="5">
+                <Text>
+                  <Text>Transaction hash:</Text>{" "}
+                  {singleTransactionInfo?.transactionHash}
+                </Text>
+                <Text mt="3">
+                  <Text>From:</Text> {singleTransactionInfo?.from}
+                </Text>
+                <Text mt="3">
+                  <Text>To:</Text> {singleTransactionInfo?.to}
+                </Text>
+                <Text mt="3">
+                  <Text>Confirmations: </Text>
+                  {singleTransactionInfo?.confirmations}
+                </Text>
+              </Flex>
 
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+              <Input
+                color="teal"
+                _placeholder={{ color: "inherit" }}
+                placeholder="Type address or ENS to see balance"
+                onChange={handleInputValue}
+                onKeyDown={handleOnKeyPressed}
+              />
 
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore the Next.js 13 playground.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+              <Button text="Click to see balance :)" onClick={handleBalance} />
+              {balance}
+            </Flex>
+          </Flex>
+          {/* </Center> */}
+        </Flex>
+      </Flex>
+    </React.Fragment>
+  );
 }
+
+export default App;
---
+++ src/app/providers.tsx
@@ -0,0 +1,20 @@
+"use client";
+
+import { CacheProvider } from "@chakra-ui/next-js";
+import { ChakraProvider, extendTheme } from "@chakra-ui/react";
+
+const theme = extendTheme({
+  fonts: {
+    heading: `'Share Tech Mono', monospace`,
+    body: `'Share Tech Mono', monospace`,
+    mono: `'Share Tech Mono', monospace`,
+  },
+});
+console.log("theme", theme);
+export function Providers({ children }: { children: React.ReactNode }) {
+  return (
+    <CacheProvider>
+      <ChakraProvider theme={theme}>{children}</ChakraProvider>
+    </CacheProvider>
+  );
+}
